a34c79031fb85eef133281684ef0547df0650282
Torsten Bøgh Köster <torsten.koester@s24.com>
2015-02-25 11:49:26 +0100

    adds simple setup example + test

---
+++ src/test/java/com/s24/search/solr/query/bmax/BmaxConfigurationTest.java
@@ -0,0 +1,24 @@
+package com.s24.search.solr.query.bmax;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.solr.util.AbstractSolrTestCase;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BmaxConfigurationTest extends AbstractSolrTestCase {
+
+   @BeforeClass
+   public static void setupBmaxCore() throws Exception {
+      BasicConfigurator.resetConfiguration();
+      BasicConfigurator.configure();
+      
+      // setup core
+      initCore("bmax-simple-solrconfig.xml", "bmax-simple-schema.xml");
+   }
+
+   @Test
+   public void testRunning() throws Exception {
+      assertTrue(true);
+   }
+
+}
