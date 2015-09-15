80e4d5ff09ca298b6385f22d98fea9b66fe2d7d3
tkaessmann <tobias.kaessmann@s24.com>
2015-09-15 11:13:08 +0200

    concatenate same debug infos
    if there is already a debuginfo for a key, it will further not be
    overwritten with the newest one.

--- src/main/java/com/s24/search/solr/util/BmaxDebugInfo.java
+++ src/main/java/com/s24/search/solr/util/BmaxDebugInfo.java
@@ -2,6 +2,7 @@ package com.s24.search.solr.util;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import org.apache.solr.common.util.NamedList;
 import org.apache.solr.handler.component.ResponseBuilder;
 
 /**
@@ -14,13 +15,34 @@ public class BmaxDebugInfo {
    private BmaxDebugInfo() {
    }
 
+   /**
+    * Add debug info to the response. Values with the same key will be concatenated.
+    * 
+    * @param rb
+    *           Where the debug info will be attached.
+    * @param key
+    *           Debug info key
+    * @param value
+    *           Debug info value
+    */
    public static void add(ResponseBuilder rb, String key, String value) {
       checkNotNull(rb, "Pre-condition violated: rb must not be null.");
       checkNotNull(key, "Pre-condition violated: key must not be null.");
       checkNotNull(value, "Pre-condition violated: value must not be null.");
 
       if (rb.isDebug()) {
-         rb.addDebugInfo(key.replace('.', '_'), value);
+         String debugKey = key.replace('.', '_');
+         String debugValue = value;
+         NamedList<Object> debugInfo = rb.getDebugInfo();
+         if (debugInfo != null) {
+            Object previousValue = debugInfo.remove(debugKey);
+            if (previousValue != null) {
+               // just add the new value if its valid (not empty and no duplicate)
+               debugValue = !((String) previousValue).contains(value) && value.length() > 0
+                     ? (String) previousValue + "; " + value : (String) previousValue;
+            }
+         }
+         rb.addDebugInfo(debugKey, debugValue);
       }
    }
 }
