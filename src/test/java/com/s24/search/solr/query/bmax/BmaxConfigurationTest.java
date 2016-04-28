006e26362afa4f014dc4c72be24e02079fa5f6a5
tkaessmann <tobias.kaessmann@s24.com>
2016-04-28 09:47:27 +0200

    Fix tests by suppressing ssl

--- src/test/java/com/s24/search/solr/query/bmax/BmaxConfigurationTest.java
+++ src/test/java/com/s24/search/solr/query/bmax/BmaxConfigurationTest.java
@@ -1,10 +1,12 @@
 package com.s24.search.solr.query.bmax;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.solr.SolrTestCaseJ4.SuppressSSL;
 import org.apache.solr.util.AbstractSolrTestCase;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+@SuppressSSL
 public class BmaxConfigurationTest extends AbstractSolrTestCase {
 
    @BeforeClass
